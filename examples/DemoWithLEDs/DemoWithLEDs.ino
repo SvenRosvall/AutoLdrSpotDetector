2cf84d126ce0bb6b2d91e8b43b9a5c3a810306b8
Sven Rosvall <sven@rosvall.ie>
2022-01-07 20:15:08 +0000

    Simplify changing detector algorithm in demo sample code.

--- examples/DemoWithLEDs/DemoWithLEDs.ino
+++ examples/DemoWithLEDs/DemoWithLEDs.ino
@@ -1,3 +1,6 @@
+// Choose what algorithm to use.
+//#define MOVING_AVERAGE_DETECTORS
+#define THRESHOLD_DETECTORS
 
 // Choose what set of output is wanted.
 #define PLOT_ALL_VALUES
@@ -6,17 +9,28 @@
 
 // Tuning parameters
 const int INTERVAL = 100; // ms
+#ifdef MOVING_AVERAGE_DETECTORS
 const float P = 0.050f;  // for moving average
 // 0.02 is too slow. Reaction time is 2-3 seconds.
-const int THRESHOLD = 150;
+const int THRESHOLD_LEVEL = 150;
 // 50 is too low. Will trigger on a train on the adjacent track.
+#endif
+#ifdef THRESHOLD_DETECTORS
+const int THRESHOLD = 850; // good level for testing indoors without sun shining through window.
+#endif
+
 // Note that the difference between open and covered is different at bright levels (low values) and dark levels (high values).
 // Sample levels for LDR 2:
 // Morning window: open: 100-120 covered: 300-350
 // Curtains: open: 350 covered: 750
 
 #include <Streaming.h>
+#ifdef MOVING_AVERAGE_DETECTORS
 #include <MovingAverageDetectors.h>
+#endif
+#ifdef THRESHOLD_DETECTORS
+#include <ThresholdDetectors.h>
+#endif
 #include <initializer_list.h>
 #include "LedChanger.h"
 
@@ -25,8 +39,12 @@ std::initializer_list<LdrLedPair> ldrLedPairs = {{A0, 10}, {A1, 9}, {A2, 8}, {A3
 
 //LedChanger ledChanger({10, 9, 8, 7, 6, 5});
 LedChanger ledChanger(ldrLedPairs);
-//MovingAverageDetectors detectors(ledChanger, {A0, A1, A2, A3, A4, A5});
-MovingAverageDetectors detectors(ledChanger, getLdrPins(ldrLedPairs));
+#ifdef MOVING_AVERAGE_DETECTORS
+MovingAverageDetectors detectors(ledChanger, {A0, A1, A2, A3, A4, A5});
+#endif
+#ifdef THRESHOLD_DETECTORS
+ThresholdDetectors detectors(ledChanger, getLdrPins(ldrLedPairs), THRESHOLD);
+#endif
 
 
 void setup() {
@@ -34,8 +52,10 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Setup()");
 
+#ifdef MOVING_AVERAGE_DETECTORS
   detectors.setMovingAverageP(P);
-  detectors.setThresholdLevel(THRESHOLD);
+  detectors.setThresholdLevel(THRESHOLD_LEVEL);
+#endif
   detectors.setup();
 
   // DEBUG("Start : " << ldrs);
