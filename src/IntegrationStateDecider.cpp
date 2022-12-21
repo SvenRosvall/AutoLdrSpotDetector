55628602dac962c7547723ce322ba702950e5a17
Sven Rosvall <sven@rosvall.ie>
2022-12-21 15:47:36 +0000

    Use scaling for darkness in IntegrationStateDecider to make it react faster in the dark.

--- src/IntegrationStateDecider.cpp
+++ src/IntegrationStateDecider.cpp
@@ -15,14 +15,21 @@ IntegrationStateDecider::IntegrationStateDecider(Factory const & factory, LdrBas
 
 LdrState IntegrationStateDecider::decide()
 {
+  // TODO: Scaling code from GroupMovingAverageLDR
+  // TODO: Make this configurable here too.
+  //const float S = parent->getThresholdScaling(); // How much to take scaling into account. 1.0 => totally. 0.0 => Use threshold level as is.
+  const float S = 0.8;
+  float scale = (1-S) + S * (1024 - ldr.value()) / 1024.0f;
+
   int diff = ldr.value() - ldr.getThreshold();
+  diff = diff / scale; // Compensate for low diffs at dark levels.
   integratedDiff += diff;
-  if (integratedDiff > limit)
+  if (integratedDiff >= limit)
   {
     integratedDiff = limit;
     return COVERED;
   }
-  else if (integratedDiff < -limit)
+  else if (integratedDiff <= -limit)
   {
     integratedDiff = -limit;
     return OPEN;
